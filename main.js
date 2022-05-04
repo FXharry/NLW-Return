b8e2fc92a271b617b121a1814084722e8a73af6a
unknown <Administrador@NG4CALLCENTER-MICHELLE.callcenter.local>
2022-05-04 13:24:25 -0300

    programming logic with javascript, primitive data string number and boolean.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('aqui')
+/*criar a função*/
+/*executar a função = chamar ela pelo nome - onScroll() */
+
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
